8d0d9acaca17adb34461211151220b4361d42236
poeyahay <poeyahayd@gmail.com>
2025-12-03 20:05:07 +0100

    fggdfgf

---
+++ Scoring with PLINK2/Merge.sh
@@ -0,0 +1,32 @@
+cd /path/to/results
+## Merge the output files together for each chromosome (to create ${trait}_Scored_File.txt)
+for trait in AF CAD Height PR BMI DCM HF SBP; do
+  # Create an empty file to store the merged results
+  > ${trait}_merged_PRS_interim.txt
+
+  # Create an interim file in which all chromosome files are concatenated
+  for i in {1..22} X Y; do
+    awk 'NR > 1 {print $1, $2}' ${trait}_Scored_File_chr${i}.sscore >> ${trait}_merged_PRS_interim.txt
+  done
+
+  # Sum scores based on matching IIDs ($1 = IID, $2 = SCORE1_SUM)
+  awk '{sum[$1] += $2} END {for (id in sum) print id, sum[id]}' ${trait}_merged_PRS_interim.txt > ${trait}_Scored_File.txt
+
+  # Add headers
+  sed -i '1i IID\tSCORE1_SUM' ${trait}_Scored_File.txt
+
+  # Remove the intermediate file
+  rm ${trait}_merged_PRS_interim.txt
+done
+
+
+## Convert to CSV (needed for Multi tool input)
+for trait in AF CAD Height PR BMI DCM HF SBP; do
+tr -s ' \t' ',' < ${trait}_Scored_File.txt > ${trait}_Scored_File.csv
+done
+
+
+# Remove the per-chromosome scored files
+for trait in AF CAD Height PR BMI DCM HF SBP; do
+rm ${trait}_Scored_File_chr*.sscore
+done
---
+++ Scoring with PLINK2/Score.sh
@@ -0,0 +1,24 @@
+## Scoring the AF trait PGS in PLINK2 per chromosome
+# --pfile: add the path to the genotype files in pgen/pvar/psam format
+# --score: add the path to the PGS score file (columns: CPRA (=chr:pos:ref:alt), SNP, A1, BETA)
+# --out: add the path and filename prefix for the output file
+
+# Score the first 15 chromosomes
+# Use 16 CPUs; 60 RAM
+for i in {1..15}; do
+    plink2 --pfile /path/to/pfiles/pfile_chr${i} \
+           --score /path/to/scripts/AF_SBayesRC_Score_File.txt 1 3 4 header cols=scoresums \
+           --out /path/to/results/AF_Scored_File_chr${i} \
+            --threads 1 &
+done
+wait
+
+# Score the last seven chromosomes
+# Use 8 CPUs; 30 RAM
+for i in {16..22}; do
+    plink2 --pfile /path/to/pfiles/pfile_chr${i} \
+           --score /path/to/scripts/AF_SBayesRC_Score_File.txt 1 3 4 header cols=scoresums \
+           --out /path/to/results/AF_Scored_File_chr${i} \
+            --threads 1 &
+done
+wait
