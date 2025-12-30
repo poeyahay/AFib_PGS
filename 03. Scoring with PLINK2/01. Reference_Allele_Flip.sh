8831e8b09ee37db7e0d7000465c24dbe59caeb31
poeyahay <poeyahayd@gmail.com>
2025-12-30 19:34:59 +0100

    gh

--- 03. Scoring with PLINK2/01. Reference_Allele_Flip.sh
+++ 03. Scoring with PLINK2/01. Reference_Allele_Flip.sh
@@ -6,7 +6,7 @@
 ## 1 ##  Create a merged reference file
 
 ## Concatenate the ID column ($3) of the .pvar files for each chromosome into one merged file
-# ID column: contains CPRA format (chr:pos:ref:alt or 10:11121:G:A)
+# ID column: contains CPRA format (chr:pos:ref:alt or e.g. 10:11121:G:A)
 for chr in {1..22} X Y; do
     cut -f3 /path/to/pfiles/pfile_${chr}.pvar
 done > pfile_merged_pvar.txt
